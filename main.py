866a8c587a61d171c69cbdb47df48ae46f4a1256
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-20 14:52:49 +0100

    Set the line width via the toolbar.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from kivy.utils import get_color_from_hex
 
 
 class CanvasWidget(Widget):
+    line_width = 2
+
     def on_touch_down(self, touch):
         # Check if any of the children components reacted to the click event
         if Widget.on_touch_down(self, touch):
@@ -14,9 +16,8 @@ class CanvasWidget(Widget):
             return
 
         with self.canvas:
-            # Specify the color of the shape to draw
-            Color(*get_color_from_hex('#0080FF80'))
-            touch.ud['current_line'] = Line(points=(touch.x, touch.y), width=2)
+            touch.ud['current_line'] = Line(points=(touch.x, touch.y),
+                                            width=self.line_width)
 
     def on_touch_move(self, touch):
         if 'current_line' in touch.ud:
@@ -32,13 +33,22 @@ class CanvasWidget(Widget):
             self.add_widget(widget)
 
     def set_color(self, new_color):
-        self.canvas.add(Color(*new_color))
+        with self.canvas:
+            Color(*new_color)
+
+    def set_line_width(self, line_width='Normal'):
+        self.line_width = {
+            'Thin': 1,
+            'Normal': 2,
+            'Thick': 4
+        }[line_width]
 
 
 class PaintApp(App):
     def build(self):
         self.canvas_widget = CanvasWidget()
-        self.canvas_widget.set_color(get_color_from_hex('#2980B9'))
+        # Set the color to be chosen at app startup
+        self.canvas_widget.set_color(get_color_from_hex('#d35400'))
 
         return self.canvas_widget
 
