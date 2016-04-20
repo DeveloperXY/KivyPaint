51bb372aededf9686bb8c82d632c2f2b7c1be2e5
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-20 10:55:39 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasWidget(Widget):
+    pass
+
+
+class PaintApp(App):
+    def build(self):
+        return CanvasWidget()
+
+
+if __name__ == '__main__':
+    PaintApp().run()
